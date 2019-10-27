c32256787483fa6240711bdeb7abe090fd6a234b
Drew11 <drew.naumenko@gmail.com>
2019-10-27 20:35:51 +0200

    Updates

---
+++ precache-manifest.6866eef503d83a3d1bc6d08f86d59814.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff66943737212c66b404f33ac1c9774",
+    "url": "/react-sapper/index.html"
+  },
+  {
+    "revision": "1ac4ecd4857973e21ee9",
+    "url": "/react-sapper/static/css/main.d4b66c7b.chunk.css"
+  },
+  {
+    "revision": "a4935fe7a3a6715b88b3",
+    "url": "/react-sapper/static/js/2.c557e9ca.chunk.js"
+  },
+  {
+    "revision": "1ac4ecd4857973e21ee9",
+    "url": "/react-sapper/static/js/main.0c207599.chunk.js"
+  },
+  {
+    "revision": "4e73c0e6476e13135d16",
+    "url": "/react-sapper/static/js/runtime-main.1b27aca4.js"
+  }
+]);
